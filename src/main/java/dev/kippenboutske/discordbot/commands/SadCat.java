a8342d41de71548c264cb81618c0174786cfb834
Kipp-ie <Jenstheminer@gmail.com>
2023-08-20 17:01:56 +0200

    Fixed it lol

--- src/main/java/dev/kippenboutske/discordbot/commands/SadCat.java
+++ src/main/java/dev/kippenboutske/discordbot/commands/SadCat.java
@@ -1,26 +1,25 @@
 package dev.kippenboutske.discordbot.commands;
 
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 
 public class SadCat extends ListenerAdapter {
-     if(event.getName().equals("sadcat")){
-        OptionMapping option = event.getOption("text");
-        String popcat = https://api.popcat.xyz/sadcat?text=
-        String text = option.replace(/ /g, '+');
+    @Override
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
+        if(event.getName().equals("sadcat")){
 
-        EmbedBuilder embed = new EmbedBuilder():
-        embed.setImage(popcat + text);
-        embed.setTitle("Here is your sad cat meme!");
+            OptionMapping option = event.getOption("text");
+            String popcat = https://api.popcat.xyz/sadcat?text=
+            String text = option.replace(/ /g, '+');
 
-        event.replyEmbeds(embed.build()).queue();
+            EmbedBuilder embed = new EmbedBuilder():
+            embed.setImage(popcat + text);
+            embed.setTitle("Here is your sad cat meme!");
 
+            event.replyEmbeds(embed.build()).queue();
+
+        }
     }
-
-
-
-
-
-
 }
